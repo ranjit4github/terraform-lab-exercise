ea5c83a010b0c16b069c66f471c1cfc50b41f90b
Ranjit Swain <58227542+ranjit4github@users.noreply.github.com>
2026-01-21 20:44:09 +0530

    Update ec2.tf

--- attachKey_EC2/ec2.tf
+++ attachKey_EC2/ec2.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 resource "aws_instance" "demoinstance" {
   ami           = "ami-091dccf4e2d272bae" # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t2.micro"
@@ -7,14 +6,4 @@ resource "aws_instance" "demoinstance" {
   tags = {
     Name = "demoinstance"
   }
-=======
-resource "aws_instance" "demoinstance" {
-  ami           = "ami-091dccf4e2d272bae" # Amazon Linux 2 AMI (HVM), SSD Volume Type
-  instance_type = "t2.micro"
-  key_name      = "demokey"
-  security_groups = [aws_security_group.sg.name]
-  tags = {
-    Name = "demoinstance"
-  }
->>>>>>> 174fc4f441b9d69be3b10ffc155ecac7476a74a5
 }
